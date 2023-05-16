edc1d4228b909d5e9e47e5ea60659c967eab63e7
alessanrolima0292@gmail.com <alessandrolima0292@gmail.com>
2023-05-15 23:30:30 -0300

    criação da seção footer e header e seus estilos

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        } else {
+            exibeElementosHeader();
+        }
+    })
     
 
+    //seção de atrações e programações das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -14,6 +28,17 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    function ocultaElementosHeader() {
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+
+    function exibeElementosHeader() {
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+    }
+
+    //seção FAQ e accordions
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
